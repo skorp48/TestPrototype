daba800415bf5e71554d19937ca1346c2989e8cb
skorp48 <alex10rat@gmail.com>
2021-06-13 16:17:09 +0300

    Vers 0.1

--- site/Handlers/main.go
+++ site/Handlers/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("site/templates/html/main.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(w, nil)
+}
 
 func main() {
-	fmt.Println("Hello Project")
+	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates/"))))
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":5000", nil)
 }
