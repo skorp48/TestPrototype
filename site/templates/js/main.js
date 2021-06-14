eb0671a08ebe4261f40d987463b9209339b9e9e1
Denadid <yasuperden@gmail.com>
2021-06-14 19:18:25 +0300

    Починил параллакс

--- site/templates/js/main.js
+++ site/templates/js/main.js
@@ -5,3 +5,10 @@ var application = new Vue({
     },
     delimiters: ['[[', ']]'],
 })
+
+    // параллакс
+    let bg = document.querySelector('.Header-image');
+	window.addEventListener('mousemove', function(e) {
+	let x = e.clientX / window.innerWidth;
+	let y = e.clientY / window.innerHeight;  
+	bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';});
